66fceee5b14e25c60488cc206fc9e6512a5a4575
nam <a@gmail.com>
2022-11-26 14:31:46 +0700

    sua imageanimals.java

--- src/main/java/ModelAnimals/ImageAnimals.java
+++ src/main/java/ModelAnimals/ImageAnimals.java
@@ -4,31 +4,38 @@ public class ImageAnimals {
 	int IdImage;
 	String NameAnimal;
 	String UrlImage;
-	
+	int tuoi;
+	String dc;
+
 	public ImageAnimals(int idImage, String nameAnimal, String urlImage) {
 		super();
 		IdImage = idImage;
 		NameAnimal = nameAnimal;
 		UrlImage = urlImage;
 	}
+
 	public int getIdImage() {
 		return IdImage;
 	}
+
 	public void setIdImage(int idImage) {
 		IdImage = idImage;
 	}
+
 	public String getNameAnimal() {
 		return NameAnimal;
 	}
+
 	public void setNameAnimal(String nameAnimal) {
 		NameAnimal = nameAnimal;
 	}
+
 	public String getUrlImage() {
 		return UrlImage;
 	}
+
 	public void setUrlImage(String urlImage) {
 		UrlImage = urlImage;
 	}
-	
 
 }
